6b8fc1b8b3237f39dccc9198c22191e7ac620d50
Omar Obregon <oobregon544@gmail.com>
2024-08-01 16:49:58 -0500

    fixing edit product

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -4,10 +4,25 @@ import {
     useActionData,
     ActionFunctionArgs,
     redirect,
+    LoaderFunctionArgs,
+    useLoaderData,
 } from 'react-router-dom';
 
 import { ErrorMessage } from '../components/ErrorMessage';
-import { addProduct } from '../services/ProductService';
+import { addProduct, getProductById } from '../services/ProductService';
+import { Product } from '../types';
+
+export async function loader({ params }: LoaderFunctionArgs) {
+    if (params.id !== undefined) {
+        const product = await getProductById(+params.id);
+
+        if (!product) {
+            redirect('/');
+        }
+
+        return product;
+    }
+}
 
 export async function action({ request }: ActionFunctionArgs) {
     const data = Object.fromEntries(await request.formData());
@@ -27,6 +42,8 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export const EditProduct = () => {
+    const product = useLoaderData() as Product;
+
     const error = useActionData() as string;
 
     return (
@@ -57,6 +74,7 @@ export const EditProduct = () => {
                         className='mt-2 block w-full p-3 bg-gray-50'
                         placeholder='Nombre del Producto'
                         name='name'
+                        defaultValue={product.name}
                     />
                 </div>
                 <div className='mb-4'>
@@ -69,6 +87,7 @@ export const EditProduct = () => {
                         className='mt-2 block w-full p-3 bg-gray-50'
                         placeholder='Precio Producto. ej. 200, 300'
                         name='price'
+                        defaultValue={product.price}
                     />
                 </div>
                 <input
